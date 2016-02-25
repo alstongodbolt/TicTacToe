4c59fb66039eea281aeecfdc5a77e2b6abb4857a
Alston Godbolt <godbolta@onid.oregonstate.edu>
2016-02-24 21:43:28 -0600

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Alston Godbolt on 2/24/16.
+//  Copyright © 2016 Alston Godbolt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
